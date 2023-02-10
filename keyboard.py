2a70ec418fde987550448e2d0cfc4ebda990c3d9
Maksym Shchur <mxm.shchur@gmail.com>
2023-02-10 18:24:59 +0200

    Created keyboard for more comfortable usage

--- keyboard.py
+++ keyboard.py
@@ -1,7 +1,18 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
 
 
-# По умолчанию False - скрывает клавиатуру после нажатия на кнопку
-keyboard = ReplyKeyboardMarkup(one_time_keyboard=True)
-keyboard.add(KeyboardButton('/jokes_about_programmers'))
-keyboard.add(KeyboardButton('/jokes_about_shtirliz'))
+keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+
+
+help_btn = KeyboardButton('/help')
+jokes_about_programmers_btn = KeyboardButton('/jokes_about_programmers')
+jokes_about_shtirlitz_btn = KeyboardButton('/jokes_about_shtirlitz')
+jokes_about_vovochka_btn = KeyboardButton('/jokes_about_vovochka')
+odesa_humor = KeyboardButton('/odesa_humor')
+donate_btn = KeyboardButton('/donate')
+
+
+keyboard.add(help_btn)
+keyboard.add(jokes_about_programmers_btn).insert(jokes_about_shtirlitz_btn)
+keyboard.add(jokes_about_vovochka_btn).insert(odesa_humor)
+keyboard.add(donate_btn)
