38301c096c5ca8496920175b02b22efe7a0748a9
Maksym Shchur <mxm.shchur@gmail.com>
2023-02-16 18:19:47 +0200

    jokes about shtirlitz are ready for use

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
+import sqlite3
+
 from aiogram import Bot, Dispatcher, executor, types
 from settings import BOT_TOKEN
 from keyboard import keyboard
+from db import sqlite_db
 
 
 HELP_COMMAND = """ 
@@ -13,6 +16,8 @@ disp = Dispatcher(bot=bot)
 
 @disp.message_handler(commands=['start'])
 async def start_handler(message: types.Message):
+    sqlite_db.start_sqlite3()
+
     user_id = message.from_user.id
     user_full_name = message.from_user.full_name
 
@@ -29,11 +34,21 @@ async def description_handler(message: types.Message):
     await message.answer('This is a comedian bot.')
 
 
+@disp.message_handler(commands=['donate'])
+async def help_handler(message: types.Message):
+    await message.answer(f'Поддержать бедного украинского разработчика: {414949934229268}')
+
+
 @disp.message_handler(commands=['jokes_about_programmers'])
 async def programmers_jokes_handler(message: types.Message):
     igor_link_programmer_sticker = 'CAACAgIAAxkBAAEHqrlj5ghUIFoSm5nkAehN41frtW5EcQACJgAD1b4hJ0mtVouBBU_ULgQ'
     await bot.send_sticker(message.from_user.id, sticker=igor_link_programmer_sticker)
     await message.answer("You've selected jokes about programmers, great choice!")
+
+
+@disp.message_handler(commands=['jokes_about_shtirlitz'])
+async def programmers_jokes_handler(message: types.Message):
+    await message.answer(await sqlite_db.sql_jokes_about_shtirlitz())
 
 
 if __name__ == '__main__':
--- db/sqlite_db.py
+++ db/sqlite_db.py
@@ -1,21 +1,49 @@
 import sqlite3 as sq
+import random
 
 
 def start_sqlite3():
-    global db, cursor
+    # global db, cursor
     db = sq.connect('test.db')
     cursor = db.cursor()
 
     if db:
         print('Connected successfully')
 
-    db.execute('CREATE TABLE IF NOT EXISTS joke_types(type_id INTEGER, joke_type TEXT)')
-    db.execute('CREATE TABLE IF NOT EXISTS jokes(joke_id INTEGER, joke TEXT)')
+    db.execute("""
+    CREATE TABLE IF NOT EXISTS jokes_shtirlitz(joke_text VARCHAR(600));
+    """)
+
+    db.execute("""
+        CREATE TABLE IF NOT EXISTS jokes_programmers(
+        joke_text VARCHAR(600));
+    """)
 
     db.commit()
 
 
-async def sql_add_command(state):
-    async with state.proxy() as data:
-        cursor.execute('INSERT INTO joke_types VALUES (?)', tuple(data.values()))
-        db.commit()
+async def sql_jokes_about_shtirlitz():
+    connect = sq.connect('test.db')
+    cursor = connect.cursor()
+
+    query = 'SELECT * FROM jokes_shtirlitz'
+
+    cursor.execute(query)
+    data = cursor.fetchall()
+
+    count_rows = 'select max(RowId) from jokes_shtirlitz'
+    cursor.execute(count_rows)
+    counter = cursor.fetchall()
+
+    jokes = []
+    for i in data:
+        jokes.append(i)
+
+    result_arr = []
+    for result in jokes:
+        result_arr.append(str(result[0]))
+
+    joke_id = random.randint(0, len(result_arr))
+    print(result_arr[joke_id], joke_id)
+    return result_arr[joke_id]
+
